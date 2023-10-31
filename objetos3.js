9591d2e857b0a12066329a5b6e59225a18108cfe
Eduardo Alvarez <opticoedu@gmail.com>
2023-10-31 13:15:03 -0300

    add objetos 3 con resoluções de Ruan, reveer!!

--- objetos3.js
+++ objetos3.js
@@ -2,16 +2,54 @@
 concluir e remover tarefas. Cada tarefa deve ser um objeto com pelo menos as
 propriedades descrição (string), data de criação (Date) e status (boolean).*/
 
-
-
-
-
+let continuar = true
+let lista = []
+while (continuar) {
+    let opcao = Number(prompt("O que deseja fazer?\n1- Adicionar tarefa, \n2- Concluir tarefa\n 3- Remover tarefa \n 4- Finalizar"))
+    switch (opcao) {
+        case 1:
+            // Adicionar
+            let descricao = prompt("Qual é a tarefa que deseja adicionar?")
+            let data = new Date(prompt("Em qual data ela se inicia")).toLocaleString()
+            let status = false
+            let objetoTarefa = {descricao, data, status}
+            lista.push(objetoTarefa)
+            console.log(lista)
+            break
+        case 2:
+            // Concluir
+            let descricaoParaConcluir = prompt("Qual tarefa deseja concluir?")
+            let indiceParaConcluir = lista.findIndex(objetoTarefa => objetoTarefa.descricao == descricaoParaConcluir)
+            if(indiceParaConcluir !== -1){
+                lista[indiceParaConcluir].status = true
+                alert("Tarefa concluida")
+            } else {
+                alert("Tarefa nao encontrada")
+            }
+            break
+        case 3:
+            // Remover
+            break
+        case 4:
+            // Finalizar
+            continuar = false
+            break
+    }
+}
 
 
 /*Faça a criação de um catálogo de filmes em JavaScript com alguns objetos possuindo as
 propriedades título, diretor, ano de lançamento e gênero.
 Em seguida dê opção para o usuário pesquisar pelo título, diretor, ano ou gênero e faça
 a filtragem, retornando os títulos em uma única string separada por vírgula.*/
+
+
+
+
+
+
+
+
 
 
 /*Crie um sistema que ajude os viajantes a planejar suas viagens. Peça para o usuário
