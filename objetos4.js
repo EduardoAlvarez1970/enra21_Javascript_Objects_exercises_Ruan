23916275d42bade2bbc8860aeb8f461bae3ee6ee
Eduardo Alvarez <opticoedu@gmail.com>
2023-10-31 13:46:22 -0300

    added objetos4 com DATE sem resolvar ainda

---
+++ objetos4.js
@@ -0,0 +1,38 @@
+
+
+/*Programe um sistema de gerenciamento de tarefas que permita aos usuários adicionar,
+concluir e remover tarefas. Cada tarefa deve ser um objeto com pelo menos as
+propriedades descrição (string), data de criação (Date) e status (boolean).*/
+
+
+
+
+/*Faça a criação de um catálogo de filmes em JavaScript com alguns objetos possuindo as
+propriedades título, diretor, ano de lançamento e gênero.
+Em seguida dê opção para o usuário pesquisar pelo título, diretor, ano ou gênero e faça
+a filtragem, retornando os títulos em uma única string separada por vírgula.*/
+
+
+
+
+
+/*Crie um sistema que ajude os viajantes a planejar suas viagens. Peça para o usuário
+inserir objetos para representar destinos, com propriedades como nome, custo
+estimado e atrações.
+Os viajantes podem escolher destinos e calcular o custo total da viagem.*/
+
+
+
+
+
+/*Crie um livro de receitas em JavaScript onde cada receita deve ser um objeto com
+propriedades como nome, ingredientes e instruções.
+Os usuários podem adicionar receitas, pesquisar por ingredientes e exibir receitas com
+base em critérios específicos.*/
+
+
+
+/*Codifique um programa que permita aos usuários registrar suas despesas diárias.
+Cada despesa deve ser representada como um objeto com propriedades como
+descrição, valor e data. Em seguida, permita que os usuários calculem o total de
+despesas para um determinado período de dias.*/
