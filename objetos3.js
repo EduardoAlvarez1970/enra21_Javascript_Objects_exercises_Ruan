26e0d911a82334b058007eff4514c6c37f6096b7
Eduardo Alvarez <opticoedu@gmail.com>
2023-10-31 14:06:36 -0300

    objetos3 algunos resueltos otros no

--- objetos3.js
+++ objetos3.js
@@ -235,18 +235,18 @@ function listarDestinosCadastrados() {
 
 
 
-
-
-
-
-
-
-
-
 /*Crie um livro de receitas em JavaScript onde cada receita deve ser um objeto com
 propriedades como nome, ingredientes e instruções.
 Os usuários podem adicionar receitas, pesquisar por ingredientes e exibir receitas com
 base em critérios específicos.*/
+
+
+
+
+
+
+
+
 
 
 /*Codifique um programa que permita aos usuários registrar suas despesas diárias.
